11f1f1d5a2581ebf218afd258e1a93629a1e070d
ludovicm67 <ludovicmuller1@gmail.com>
2019-11-26 12:01:13 +0100

    initial commit

---
+++ app/src/main/java/fr/ludovicm67/eventmanager/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package fr.ludovicm67.eventmanager.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
