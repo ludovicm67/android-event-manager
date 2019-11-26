fea9a471028eb9decd2288b59cbe56ef8e3fe20c
ludovicm67 <ludovicmuller1@gmail.com>
2019-11-26 16:44:05 +0100

    colors: fix statusbar color

--- app/src/main/java/fr/ludovicm67/eventmanager/AuthActivity.kt
+++ app/src/main/java/fr/ludovicm67/eventmanager/AuthActivity.kt
@@ -2,6 +2,9 @@ package fr.ludovicm67.eventmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import fr.ludovicm67.eventmanager.ui.register.RegisterFragment
 
 class AuthActivity : AppCompatActivity() {
@@ -9,6 +12,12 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.auth_activity)
+
+        val w: Window = window
+        w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        w.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.auth_container, RegisterFragment.newInstance())
